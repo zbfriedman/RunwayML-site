d6ec61347462c1000b5ace3cf6c19c11e6044049
zbfriedman <46681068+zbfriedman@users.noreply.github.com>
2021-09-21 13:36:58 -0500

    f

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const imgArr = [
     "triangle/trianglegif.gif",
     "eager/eagergif.gif",
     "tired/tiredgif.gif",
-    "anxious/anxiousgif.gif",
+    "anxious/anxiousgif2.gif",
     "excited/excitedgif.gif",
     "happy/happygif.gif",
     "sad/sadgif.gif",
@@ -17,12 +17,12 @@ const imgArr = [
     "cone/conegif.gif",
     "cube/cubegif.gif",
     "aurora/auroragif.gif",
-    "lake/lakegif.gif",
+    "lake/lakegif2.gif",
     "mountain/mountaingif.gif",
     "lightning/lightninggif.gif",
     "ice/icegif.gif",
     "geometric/geometricgif.gif",
-    "book/bookgif.gif",
+    "book/bookgif2.gif",
     "poem/poemgif.gif",
     ];
 
@@ -83,40 +83,50 @@ const wordArr = [
     ];
 
     //this shows text, art, gif
-    const num = Math.floor(Math.random()*artArr.length);
-    text();
-    art();
-    gif();
+const num = Math.floor(Math.random()*artArr.length);
+var preHeight = (Math.floor(Math.random() * 300) + 10);
+if (preHeight < 100){preHeight = preHeight + 100;}
+const height = String(preHeight + "px");
+const topp = String((Math.floor(Math.random() * (window.innerHeight - preHeight - 50)) + 10) + "px");
+const leftArt = String((Math.floor(Math.random() * (window.innerWidth/4 - preHeight - 10)) + 10) + "px");
+const leftImage = String((Math.floor(window.innerWidth/3 + Math.random() * 100) + "px"));
+let intViewportWidth = window.innerWidth;
+console.log(height);
 
-    function text(){
-        const word = document.createTextNode(wordArr[num]);
-        document.getElementById('images').style.color = 'white';
-        document.getElementById('images').style.fontSize = '30px';
-        document.getElementById('images').appendChild(word);
-    }
-    function art(){
-        const art = document.createElement('img');
-        art.src = artArr[num];
-        art.style.margin = "120px";
-        art.style.border = "1px solid black";
-        document.getElementById('images').appendChild(art);
-        console.log(artArr[num])
-    }
+const art = document.createElement('img');
+art.src = artArr[num];
+art.style.position = 'absolute';
+art.style.top = topp;
+art.style.height = height;
+art.style.left = leftArt;
+art.style.border = "1px solid black";
+document.getElementById('images').appendChild(art);
+console.log(artArr[num])
 
-    function gif(){
-        const image = document.createElement('img');
-        image.src = imgArr[num];
-        image.style.width = "700px";
-        image.style.border = "1px solid black";
-        document.getElementById('images').appendChild(image);
-    }
+const image = document.createElement('img');
+image.src = imgArr[num];
+image.style.position = 'absolute';
+image.style.top = topp;
+image.style.height = height;
+image.style.left = leftImage;
+image.style.border = "1px solid black";
+document.getElementById('images').appendChild(image);
+
+// var x = 0;
+// var y = 0;
+// document.addEventListener('mousemove', (event) => {
+// 	x = event.clientX;     // Get the horizontal coordinate
+//     y = event.clientY;     // Get the vertical coordinate
+//     console.log(x);
+// });
 
 
-/*     //this prints them all out in a grid
-    for (let i = 0; i < imgArr.length; i++) {
-        const image = document.createElement('img');
-        image.src = imgArr[i];
-        image.style.width = "250px";
-        image.style.height = "150px";
-        document.getElementById('images').appendChild(image);        
-    } */
+/* //this prints them all out in a grid
+for (let i = 0; i < imgArr.length; i++) {
+    const image = document.createElement('img');
+    image.src = imgArr[i];
+    image.style.width = "250px";
+    image.style.height = "150px";
+    document.getElementById('images').appendChild(image);    
+} */
+
